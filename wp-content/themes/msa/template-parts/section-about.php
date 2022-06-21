016b3fccbc28c111d96a644651f5793618a4541d
Shantiram <Shantiramtiwari0852@gmail.com>
2022-06-21 12:34:26 +0545

    updated

--- wp-content/themes/msa/template-parts/section-about.php
+++ wp-content/themes/msa/template-parts/section-about.php
@@ -2,15 +2,17 @@
 $post_id = getIdBySlug('section-about','sections');
 $content = get_post_field('post_content', $post_id);
 $section_title = get_field('section_title',$post_id);
- $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'single-post-thumbnail' );
+ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'full' );
 //?>
 <section class="about-section ks-spb ks-spt">
     <div class="container">
         <div class="about-grid-container">
             <div class="left-grid">
+                <?php if($image): ?> 
                 <figure>
                     <img src="<?php echo $image[0] ?>" class="img-fluid cover-image" alt="">
                 </figure>
+                 <?php endif ?> 
             </div>
             <div class="right-grid">
                 <span class="horizontal-title-bar"></span>
