191de2d4bc9d2e2e82a2b1f68fe49a33c737f37c
sanjeev <sanjeev@kyanitesoftware.com>
2022-06-20 20:55:58 -0800

    done

--- wp-content/themes/msa/single-projects.php
+++ wp-content/themes/msa/single-projects.php
@@ -10,35 +10,39 @@ $project_gallery = CFS()->get('gallery');
         <?php
 
         //  Section Hero Start
-//        get_template_part( 'template-parts/section', 'hero' );
+        //        get_template_part( 'template-parts/section', 'hero' );
         //  Section Hero End
 
         // Content Service Starts
-//        get_template_part( 'template-parts/project/content', 'project' );
+        //        get_template_part( 'template-parts/project/content', 'project' );
         // Content Service Starts
-?>
+        ?>
 
-<!--        Featured Project    -->
+        <!--        Featured Project    -->
         <section class="project-section ks-smb ks-smt">
             <div class="container">
                 <h2 class="section-title">Chapel Rd, Bankstown, St. Felix Primary School, New Pedestrian Crossing</h2>
                 <div class="project-slider--container">
-                    <div class="arrows left prev3"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/angle-left.png" class="img-fluid" alt=""/></div>
-                    <div class="arrows right next3"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/angle-right.png" class="img-fluid" alt=""/></div>
-                        <div id="project-slider">
-                            <?php
-                            if($project_gallery):
-                                foreach ($project_gallery as $item):
-                                    ?>
-                            <figure class="mb-0 project-slider--image-container">
-                                <img src="<?php echo $item['image']; ?>" alt="<?php echo $item['caption']; ?>"
-                                     class="img-fluid cover-image">
-                            </figure>
+                    <div class="arrows left prev3"><img
+                                src="<?php echo get_template_directory_uri(); ?>/assets/images/angle-left.png"
+                                class="img-fluid" alt=""/></div>
+                    <div class="arrows right next3"><img
+                                src="<?php echo get_template_directory_uri(); ?>/assets/images/angle-right.png"
+                                class="img-fluid" alt=""/></div>
+                    <div id="project-slider">
+                        <?php
+                        if ($project_gallery):
+                            foreach ($project_gallery as $item):
+                                ?>
+                                <figure class="mb-0 project-slider--image-container">
+                                    <img src="<?php echo $item['image']; ?>" alt="<?php echo $item['caption']; ?>"
+                                         class="img-fluid cover-image">
+                                </figure>
                             <?php
                             endforeach;
                         endif;
                         ?>
-                        </div>
+                    </div>
                 </div>
                 <div class="project-content-flex">
                     <div class="single-project--flex-item">
@@ -58,22 +62,22 @@ $project_gallery = CFS()->get('gallery');
                     </div>
                     <div class="single-project--flex-item">
                         <h2 class="single-project--subtitle">
-                           Client:
+                            Client:
                         </h2>
                         <figure class="mb-0">
                             <img src="<?php echo $client['url']; ?>" alt="<?php echo $client['alt']; ?>"
-                            class="img-fluid cover-image">
+                                 class="img-fluid cover-image">
                         </figure>
 
                     </div>
                 </div>
             </div>
         </section>
-<!--        Featured  Project   -->
+        <!--        Featured  Project   -->
 
         <?php
         // Section Projects Starts
-        get_template_part( 'template-parts/section', 'project' );
+        get_template_part('template-parts/section', 'project');
         // Section Projects Ends
 
         ?>
